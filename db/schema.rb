b193eaa1c5b701f46d1e39c5341c525b9b57fd96
Charles Lowell <cowboyd@thefrontside.net>
2013-07-15 15:25:32 -0500

    use ruby js engine, devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418114036) do
+ActiveRecord::Schema.define(:version => 20130715201817) do
 
   create_table "ideas", :force => true do |t|
     t.string   "name"
@@ -20,5 +20,20 @@ ActiveRecord::Schema.define(:version => 20120418114036) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",               :default => "", :null => false
+    t.string   "encrypted_password",  :default => "", :null => false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
